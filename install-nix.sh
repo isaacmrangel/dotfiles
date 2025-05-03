6e19dcd665b2a4f686b04cea1147213a1600a366
Gadiel Rangel <140253338+gadielrangel@users.noreply.github.com>
2025-05-03 16:20:34 -0300

    Update install-nix.sh

--- install-nix.sh
+++ install-nix.sh
@@ -1,1 +1,1 @@
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+sudo apt install curl && curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
